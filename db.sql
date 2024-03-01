7bf91cd850100e222a52370b5ded7a317e80e465
kaue gatto <kauegatto123@gmail.com>
2024-03-01 16:58:18 -0300

    add db sql

---
+++ db.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Conta (
+                       Email VARCHAR(255) PRIMARY KEY,
+                       Nome VARCHAR(255)
+)
+
+
+CREATE TABLE Cliente (
+                         Nome VARCHAR(255),
+                         Created_At DATETIME,
+                         Email_Conta VARCHAR(255),
+                         UrlAvatar VARCHAR(255),
+                         CONSTRAINT FK_EmailClient_Conta FOREIGN KEY (Email_Conta)
+                             REFERENCES Conta(Email),
+                         CONSTRAINT PK_Nome_Email PRIMARY KEY(Email_Conta, Nome)
+)
