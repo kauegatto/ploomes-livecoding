37f3da3d7704a689316ab583e05426020b102d27
kaue gatto <kauegatto123@gmail.com>
2024-03-01 17:06:28 -0300

    refactor: remove extra spaces for db.sql

--- db.sql
+++ db.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE Conta (
-                       Email VARCHAR(255) PRIMARY KEY,
-                       Nome VARCHAR(255)
+    Email VARCHAR(255) PRIMARY KEY,
+    Nome VARCHAR(255)
 )
 
+INSERT INTO Conta Values ('ploomes@email.com', 'Ploomes')
 
 CREATE TABLE Cliente (
-                         Nome VARCHAR(255),
-                         Created_At DATETIME,
-                         Email_Conta VARCHAR(255),
-                         UrlAvatar VARCHAR(255),
-                         CONSTRAINT FK_EmailClient_Conta FOREIGN KEY (Email_Conta)
-                             REFERENCES Conta(Email),
-                         CONSTRAINT PK_Nome_Email PRIMARY KEY(Email_Conta, Nome)
+    Nome VARCHAR(255),
+    Created_At DATETIME,
+    Email_Conta VARCHAR(255),
+    UrlAvatar VARCHAR(255),
+    CONSTRAINT FK_EmailClient_Conta FOREIGN KEY (Email_Conta)
+     REFERENCES Conta(Email),
+    CONSTRAINT PK_Nome_Email PRIMARY KEY(Email_Conta, Nome)
 )
